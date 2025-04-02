41fa3f1cc849eb09ab0e395b2f13a47663647867
Anton Rauchenberger <toni.rauchenberger03@gmail.com>
2025-04-02 09:56:28 +0200

    🔧 Added user loaction to map and loading screen

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, View, PermissionsAndroid, Platform } from "react-native";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
 import Header from "@/components/Header";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
+import constants from "../consts";
 
 export default function Map() {
-    const [userLocation, setUserLocation] =
-        useState<Location.LocationObject | null>(null);
+    const [userLocation, setUserLocation] = useState<any>(null);
 
     useEffect(() => {
         async function getCurrentLocation() {
@@ -22,48 +22,61 @@ export default function Map() {
         getCurrentLocation();
     }, []);
 
-    useEffect(() => {
-        console.log(userLocation);
-    }, [userLocation]);
-
     const styles = StyleSheet.create({
         mapContainer: {
-            width: "95%",
+            width: "97%",
             height: 230,
             margin: "auto",
             borderRadius: 15,
             overflow: "hidden",
             borderWidth: 2,
-            borderColor: "lightgray",
+            borderColor: constants.FONT_COLOR,
         },
         map: {
             width: "100%",
             height: "100%",
         },
+        mapLoadingScreen: {
+            backgroundColor: constants.BACKGROUND_COLOR,
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+        },
     });
 
     return (
-        <View>
+        <View style={{ backgroundColor: constants.FONT_COLOR }}>
             <Header />
             <View style={styles.mapContainer}>
-                <MapView
-                    style={styles.map}
-                    initialRegion={{
-                        latitude: userLocation?.latitude || 37.78825,
-                        longitude: userLocation?.longitude || -122.4324,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
-                    }}
-                >
-                    {userLocation && (
+                {userLocation ? (
+                    <MapView
+                        style={styles.map}
+                        initialRegion={{
+                            latitude: userLocation?.coords?.latitude || 49.0029,
+                            longitude:
+                                userLocation?.coords?.longitude || 12.0957,
+                            latitudeDelta: 0.0922,
+                            longitudeDelta: 0.0421,
+                        }}
+                    >
                         <Marker
                             coordinate={{
-                                latitude: userLocation?.latitude,
-                                longitude: userLocation?.longitude,
+                                latitude: userLocation?.coords?.latitude,
+                                longitude: userLocation?.coords?.longitude,
                             }}
+                            title={"Dein Standort"}
                         />
-                    )}
-                </MapView>
+                    </MapView>
+                ) : (
+                    <View style={styles.mapLoadingScreen}>
+                        <ActivityIndicator
+                            size={"small"}
+                            color={constants.PRIMARY_COLOR}
+                        />
+                    </View>
+                )}
             </View>
         </View>
     );
