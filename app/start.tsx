a07cbacac0516ee0a13d7d75fb7bfb6b0dc9aa5c
Anton Rauchenberger <toni.rauchenberger03@gmail.com>
2025-04-13 23:37:13 +0200

    ➕ Added start page

--- app/start.tsx
+++ app/start.tsx
@@ -1,23 +1,65 @@
 import { router } from "expo-router";
-import { Text, View } from "react-native";
-import { useSession } from "@/lib/Authentification/ctx";
+import { Text, View, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import constants from "./consts";
+import SlideToUnlock from "@/components/Authentification/SlideToUnlock";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function Start() {
-    const { signIn } = useSession();
+    const [unlocked, setUnlocked] = useState(false);
+
+    useEffect(() => {
+        if (unlocked) {
+            router.navigate("/signIn");
+        }
+    }, [unlocked]);
+
+    const styles = StyleSheet.create({
+        wrapper: {
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: constants.BACKGROUND_COLOR,
+            height: "100%",
+            width: "100%",
+            padding: 35,
+        },
+        imageContainer: {
+            marginBottom: 25,
+        },
+        header: {
+            fontSize: 30,
+            marginBottom: 25,
+            fontWeight: "600",
+            color: constants.TEXT_COLOR,
+            width: "100%",
+        },
+        description: {
+            fontSize: 15,
+            fontWeight: "300",
+            color: constants.PRIMARY_COLOR,
+            marginBottom: 45,
+            width: "100%",
+        },
+    });
+
     return (
-        <View
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
-            <Text
-                onPress={() => {
-                    signIn();
-                    // Navigate after signing in. You may want to tweak this to ensure sign-in is
-                    // successful before navigating.
-                    router.replace("/(tabs)");
-                }}
-            >
-                Start
+        <View style={styles.wrapper}>
+            <View style={styles.imageContainer}>
+                <Ionicons
+                    name="paw-outline"
+                    size={300}
+                    color={constants.TEXT_COLOR}
+                />
+            </View>
+            <Text style={styles.header}>
+                Gemeinsam unterwegs - immer verbunden
             </Text>
+            <Text style={styles.description}>
+                Behalte deinen Hund im Blick, teile schöne Momente und bleibe
+                immer verbunden.
+            </Text>
+            <SlideToUnlock setUnlocked={setUnlocked} />
         </View>
     );
 }
