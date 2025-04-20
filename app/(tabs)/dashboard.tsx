d67de493d01922c5b848360eb22f82f8589d4f90
Anton Rauchenberger <toni.rauchenberger03@gmail.com>
2025-04-21 00:00:54 +0200

    ➕ Added cards and live tracker sceleton

--- app/(tabs)/dashboard.tsx
+++ app/(tabs)/dashboard.tsx
@@ -1,15 +1,102 @@
-import { Text, View } from "react-native";
+import React, { useEffect, useRef } from "react";
+import {
+    Text,
+    View,
+    StyleSheet,
+    TouchableOpacity,
+    Animated,
+} from "react-native";
+import constants from "../consts";
+import StatCards from "@/components/Tabs/dashboard/StatCards";
+import { router } from "expo-router";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 
 export default function Dashboard() {
+    const fadeAnim = useRef(new Animated.Value(1)).current;
+
+    useEffect(() => {
+        Animated.loop(
+            Animated.sequence([
+                Animated.timing(fadeAnim, {
+                    toValue: 0,
+                    duration: 500,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(fadeAnim, {
+                    toValue: 1,
+                    duration: 500,
+                    useNativeDriver: true,
+                }),
+            ])
+        ).start();
+    }, [fadeAnim]);
+
+    const styles = StyleSheet.create({
+        container: {
+            height: 100,
+        },
+        background: {
+            height: "80%",
+            backgroundColor: constants.BACKGROUND_COLOR,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        headline: {
+            color: constants.TEXT_COLOR,
+            fontSize: 20,
+            marginTop: 31,
+        },
+        tracker: {
+            width: "97%",
+            margin: "auto",
+            height: 60,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: 15,
+            borderWidth: 3,
+            borderColor: constants.TEXT_COLOR,
+            marginTop: 10,
+        },
+        trackerText: {
+            color: constants.TEXT_COLOR,
+        },
+        trackerContainer: {
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 8,
+        },
+    });
+
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text>Dashboard</Text>
+        <View style={{ backgroundColor: constants.FONT_COLOR }}>
+            <View style={styles.container}>
+                <View style={styles.background}>
+                    <Text style={styles.headline}>Dashboard</Text>
+                </View>
+            </View>
+            <StatCards />
+            <TouchableOpacity
+                style={styles.tracker}
+                activeOpacity={0.8}
+                onPress={() => router.navigate("/(tabs)/map")}
+            >
+                <View style={styles.trackerContainer}>
+                    <Animated.View style={{ opacity: fadeAnim }}>
+                        <FontAwesome6
+                            name="map-location-dot"
+                            size={24}
+                            color={constants.TEXT_COLOR}
+                        />
+                    </Animated.View>
+                    <Text style={styles.trackerText}>
+                        <Text style={{ fontWeight: "bold" }}>{2} km</Text> von
+                        dir entfernt
+                    </Text>
+                </View>
+            </TouchableOpacity>
         </View>
     );
 }
---
+++ components/Tabs/dashboard/StatCards.tsx
@@ -0,0 +1,145 @@
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import constants from "@/app/consts";
+import { router } from "expo-router";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import Foundation from "@expo/vector-icons/Foundation";
+
+export default function StatCards() {
+    const styles = StyleSheet.create({
+        container: {
+            backgroundColor: constants.TEXT_COLOR,
+            padding: 15,
+            borderRadius: 15,
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: 3 },
+            shadowOpacity: 0.2,
+            shadowRadius: 4,
+            elevation: 4,
+            zIndex: 10,
+            width: "97%",
+            margin: "auto",
+        },
+        row: {
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+        },
+        card: {
+            backgroundColor: constants.BACKGROUND_COLOR,
+            width: 166,
+            borderRadius: 8,
+            height: 120,
+            padding: 7,
+        },
+        header: {
+            color: constants.TEXT_COLOR,
+            minHeight: 50,
+        },
+        value: {
+            color: constants.TEXT_COLOR,
+            fontSize: 20,
+            fontWeight: "500",
+            marginLeft: 20,
+        },
+        description: {
+            fontSize: 10,
+            opacity: 0.8,
+        },
+    });
+
+    return (
+        <View style={styles.container}>
+            <View style={[styles.row, { marginBottom: 15 }]}>
+                <TouchableOpacity
+                    style={styles.card}
+                    activeOpacity={0.9}
+                    onPress={() => router.navigate("/(tabs)/map")}
+                >
+                    <Text style={styles.header}>
+                        Meldungen{" "}
+                        <Text style={styles.description}>
+                            (in den letzten 2 Wochen)
+                        </Text>
+                    </Text>
+                    <Text style={styles.value}>
+                        {20}{" "}
+                        <View
+                            style={{ transform: "translate3d(-2px, 3px, 0px)" }}
+                        >
+                            <Ionicons
+                                name="warning"
+                                size={20}
+                                color={constants.TEXT_COLOR}
+                            />
+                        </View>
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.card}
+                    activeOpacity={0.9}
+                    onPress={() => router.navigate("/(tabs)/camera")}
+                >
+                    <Text style={styles.header}>Gemachte Fotos</Text>
+                    <Text style={styles.value}>
+                        {20}{" "}
+                        <View
+                            style={{ transform: "translate3d(-2px, 3px, 0px)" }}
+                        >
+                            <MaterialIcons
+                                name="camera"
+                                size={20}
+                                color={constants.TEXT_COLOR}
+                            />
+                        </View>
+                    </Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.row}>
+                <TouchableOpacity
+                    style={styles.card}
+                    activeOpacity={0.9}
+                    onPress={() => router.navigate("/(tabs)/map")}
+                >
+                    <Text style={styles.header}>Häufchen-Counter</Text>
+                    <Text style={styles.value}>
+                        {20}{" "}
+                        <View>
+                            <FontAwesome5
+                                name="poop"
+                                size={17}
+                                color={constants.TEXT_COLOR}
+                            />
+                        </View>
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.card}
+                    activeOpacity={0.9}
+                    onPress={() => router.navigate("/(tabs)/map")}
+                >
+                    <Text style={styles.header}>
+                        Gelaufene Meter{" "}
+                        <Text style={styles.description}>
+                            (in den letzten 2 Wochen)
+                        </Text>
+                    </Text>
+                    <Text style={styles.value}>
+                        {2187}{" "}
+                        <View
+                            style={{ transform: "translate3d(-2px, 5px, 0px)" }}
+                        >
+                            <Foundation
+                                name="guide-dog"
+                                size={24}
+                                color={constants.TEXT_COLOR}
+                            />
+                        </View>
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
