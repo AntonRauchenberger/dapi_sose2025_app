d2e3db4d9cee05b8af7c379d2ec03faff0a0c2e1
Anton Rauchenberger <toni.rauchenberger03@gmail.com>
2025-04-28 00:34:36 +0200

    ➕ Added poop button

--- components/Tabs/map/RecordButton.tsx
+++ components/Tabs/map/RecordButton.tsx
@@ -7,6 +7,7 @@ import {
     Animated,
     Easing,
 } from "react-native";
+import { Audio } from "expo-av";
 import Foundation from "@expo/vector-icons/Foundation";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import Feather from "@expo/vector-icons/Feather";
@@ -14,15 +15,74 @@ import * as Haptics from "expo-haptics";
 import { useState, useRef, useEffect } from "react";
 import LottieView from "lottie-react-native";
 import particles from "@/assets/animations/particles.json";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as Location from "expo-location";
+import Firebase from "@/lib/Firebase/Firebase";
+import { doc, setDoc } from "firebase/firestore";
 
 export default function RecordButton() {
     const [isReccording, setIsReccording] = useState(false);
     const [finishedRoute, setFinishedRoute] = useState(false);
+    const [poopButtonEnabled, setPoopButtonEnabled] = useState(true);
+    const [pooped, setPooped] = useState(false);
     const scaleAnim = useRef(new Animated.Value(1)).current;
     const particleRef = useRef(null);
 
+    const playSound = async () => {
+        const { sound } = await Audio.Sound.createAsync(
+            require("../../../assets/sounds/smallPoop.mp3")
+        );
+        await sound.playAsync();
+    };
+
     const saveRoute = () => {
         // TODO
+    };
+
+    const savePoop = async () => {
+        async function getCurrentLocation() {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                return;
+            }
+            const location = await Location.getCurrentPositionAsync({});
+            return {
+                latitude: location?.coords?.latitude,
+                longitude: location?.coords?.longitude,
+            };
+        }
+        const savePoopMarkerList = async (list: []) => {
+            try {
+                const jsonValue = JSON.stringify(list);
+
+                // firestore
+                const user = Firebase.auth?.currentUser;
+                if (!user) throw new Error("no user logged in");
+                const userPoopRef = doc(Firebase.db, "poopMarkers", user.uid);
+                await setDoc(userPoopRef, list);
+
+                // async storage
+                await AsyncStorage.setItem("poopMarkerList", jsonValue);
+            } catch (e) {
+                console.error("Fehler beim Speichern:", e);
+            }
+        };
+        const getPoopMarkerList = async () => {
+            try {
+                const jsonValue = await AsyncStorage.getItem("poopMarkerList");
+                return jsonValue != null ? JSON.parse(jsonValue) : [];
+            } catch (e) {
+                console.error("Fehler beim Laden:", e);
+                return [];
+            }
+        };
+
+        const currentLocation = await getCurrentLocation();
+        const poopMarkerList = await getPoopMarkerList();
+        poopMarkerList.push(currentLocation);
+        await savePoopMarkerList(poopMarkerList);
     };
 
     const handleClick = () => {
@@ -36,6 +96,20 @@ export default function RecordButton() {
                 setFinishedRoute(false);
             }, 1500);
         }
+    };
+
+    const handlePoopButton = async () => {
+        playSound();
+        setPooped(true);
+
+        savePoop();
+        await savePoop();
+        await AsyncStorage.setItem("lastPoopTime", Date.now().toString());
+
+        setTimeout(() => {
+            setPooped(false);
+            setPoopButtonEnabled(false);
+        }, 1000);
     };
 
     useEffect(() => {
@@ -62,8 +136,40 @@ export default function RecordButton() {
         }
     }, [isReccording]);
 
+    // checks if poop button can be pressed (once per day)
+    useEffect(() => {
+        const handle = async () => {
+            try {
+                const lastPoopTime = await AsyncStorage.getItem("lastPoopTime");
+                if (lastPoopTime) {
+                    const lastPoop = parseInt(lastPoopTime, 10);
+                    const now = Date.now();
+                    const diff = now - lastPoop;
+
+                    if (diff > 24 * 60 * 60 * 1000) {
+                        setPoopButtonEnabled(true);
+                    } else {
+                        setPoopButtonEnabled(false);
+                    }
+                } else {
+                    setPoopButtonEnabled(true);
+                }
+            } catch (e) {
+                console.error("Fehler beim Prüfen:", e);
+                setPoopButtonEnabled(true);
+            }
+        };
+
+        handle();
+    }, []);
+
     const styles = StyleSheet.create({
         container: {
+            display: "flex",
+            flexDirection: "row",
+            width: isReccording ? "60%" : "auto",
+        },
+        recordButtonContainer: {
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
@@ -98,52 +204,86 @@ export default function RecordButton() {
 
     return (
         <View style={styles.container}>
-            <View style={constants.SHADOW_STYLE}>
+            {isReccording && (
                 <TouchableOpacity
-                    style={styles.button}
-                    onPress={handleClick}
+                    style={[
+                        styles.button,
+                        {
+                            width: 60,
+                            height: 60,
+                            marginTop: 10,
+                            backgroundColor: constants.SECCONDARY_COLOR,
+                            borderWidth: 2,
+                            opacity: poopButtonEnabled ? 1 : 0.75,
+                        },
+                    ]}
+                    onPress={handlePoopButton}
                     activeOpacity={0.9}
+                    disabled={!poopButtonEnabled}
                 >
-                    <LottieView
-                        ref={particleRef}
-                        source={particles}
-                        autoPlay={false}
-                        loop={false}
-                        style={styles.lottie}
-                    />
-
-                    {finishedRoute ? (
-                        <Feather
-                            name="check"
-                            size={40}
+                    {pooped ? (
+                        <MaterialCommunityIcons
+                            name="emoticon-poop"
+                            size={28}
                             color={constants.TEXT_COLOR}
                         />
-                    ) : isReccording ? (
-                        <Animated.View
-                            style={{ transform: [{ scale: scaleAnim }] }}
-                        >
-                            <FontAwesome5
-                                name="route"
-                                size={30}
-                                color={constants.TEXT_COLOR}
-                            />
-                        </Animated.View>
                     ) : (
-                        <Foundation
-                            name="guide-dog"
-                            size={60}
+                        <FontAwesome6
+                            name="poop"
+                            size={24}
                             color={constants.TEXT_COLOR}
                         />
                     )}
                 </TouchableOpacity>
+            )}
+            <View style={styles.recordButtonContainer}>
+                <View style={constants.SHADOW_STYLE}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={handleClick}
+                        activeOpacity={0.9}
+                    >
+                        <LottieView
+                            ref={particleRef}
+                            source={particles}
+                            autoPlay={false}
+                            loop={false}
+                            style={styles.lottie}
+                        />
+
+                        {finishedRoute ? (
+                            <Feather
+                                name="check"
+                                size={40}
+                                color={constants.TEXT_COLOR}
+                            />
+                        ) : isReccording ? (
+                            <Animated.View
+                                style={{ transform: [{ scale: scaleAnim }] }}
+                            >
+                                <FontAwesome5
+                                    name="route"
+                                    size={30}
+                                    color={constants.TEXT_COLOR}
+                                />
+                            </Animated.View>
+                        ) : (
+                            <Foundation
+                                name="guide-dog"
+                                size={60}
+                                color={constants.TEXT_COLOR}
+                            />
+                        )}
+                    </TouchableOpacity>
+                </View>
+                <Text style={styles.buttonText}>
+                    {finishedRoute
+                        ? "Route wurde gespeichert"
+                        : isReccording
+                        ? "Route beenden"
+                        : "Route starten"}
+                </Text>
             </View>
-            <Text style={styles.buttonText}>
-                {finishedRoute
-                    ? "Route wurde gespeichert"
-                    : isReccording
-                    ? "Route beenden"
-                    : "Route starten"}
-            </Text>
         </View>
     );
 }
