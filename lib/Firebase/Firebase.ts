d816e110bca818ff5411de5e4654550b6c226d56
Anton Rauchenberger <toni.rauchenberger03@gmail.com>
2025-05-05 12:31:00 +0200

    🐛 Fixed firestore auth component bug with metro config

--- lib/Firebase/Firebase.ts
+++ lib/Firebase/Firebase.ts
@@ -1,31 +1,28 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, initializeAuth } from "firebase/auth";
 import firebaseConfig from "./firebaseConfig";
-
 export default class Firebase {
     static db: any;
     static auth: any;
     static app: any;
 
+    // TODO Fix Firebase error
     static init() {
-        try {
-            if (!Firebase.app) {
-                Firebase.app = initializeApp(firebaseConfig);
-                console.log("Firebase app initialized.");
-            }
+        if (!Firebase.app) {
+            Firebase.app = initializeApp(firebaseConfig);
+            console.log("Firebase app initialized.");
+        }
 
-            if (!Firebase.db) {
-                Firebase.db = getFirestore(Firebase.app);
-                console.log("Firestore initialized.");
-            }
+        if (!Firebase.db) {
+            Firebase.db = getFirestore(Firebase.app);
+            console.log("Firestore initialized.");
+        }
 
-            if (!Firebase.auth) {
-                Firebase.auth = getAuth(Firebase.app);
-                console.log("Firebase Auth initialized.");
-            }
-        } catch (error) {
-            console.error("Error initializing Firebase:", error);
+        if (!Firebase.auth) {
+            Firebase.auth = initializeAuth(Firebase.app);
+            Firebase.auth = getAuth(Firebase.app);
+            console.log("Firebase Auth initialized.");
         }
     }
 }
